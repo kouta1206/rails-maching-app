c73d97ec79f7dd9786a2889e77c0600a3bf1b329
kouta1206 <ko.bump1206@icloud.com>
2022-10-01 15:29:57 +0900

    ユーザー編集機能作成

--- db/migrate/20220928145539_add_self_introduction_to_user.rb
+++ db/migrate/20220928145539_add_self_introduction_to_user.rb
@@ -1,5 +1,5 @@
 class AddSelfIntroductionToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :self_introcution, :text
+    add_column :users, :self_introduction, :text, null: true
   end
 end
